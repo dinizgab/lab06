a699a191a0cf74b94e12b1aaa3f94a228bd6497f
Sabrinabs <sabrina.silva@ccc.ufcg.edu.br>
2022-08-12 16:22:14 -0300

    Definindo as funcionalidades de Tarefa em Atividade

--- src/sapo/atividade/AtividadeService.java
+++ src/sapo/atividade/AtividadeService.java
@@ -1,82 +1,82 @@
 package sapo.atividade;
 
+import java.util.Map;
+
 import sapo.pessoa.PessoaService;
 
 public class AtividadeService {
-    private PessoaService ps;
+	private PessoaService ps;
 
-    public AtividadeService(PessoaService ps) {
-        this.ps = ps;
-    }
+	public AtividadeService(PessoaService ps) {
+		this.ps = ps;
+	}
 
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-   
-    // tarefa
+	
+	// tarefa
 	public String cadastraTarefa(String atividadeId, String nome, String[] habilidades) {
-		// TODO Auto-generated method stub
-		return null;
+		return atividades.get(atividadeId).cadastraTarefa(nome, habilidades);
 	}
 
 	public void alterarNomeTarefa(String tarefaId, String novoNome) {
-		// TODO Auto-generated method stub
-		
+		recuperaAtividade(tarefaId).alteraNomeTarefa(tarefaId, novoNome);
+
 	}
 
 	public void alterarHabilidadesTarefa(String idTarefa, String[] habilidades) {
-		// TODO Auto-generated method stub
-		
+		recuperaAtividade(idTarefa).alterarHabilidadesTarefa(idTarefa, habilidades);
 	}
 
 	public void adicionarHorasTarefa(String idTarefa, int horas) {
-		// TODO Auto-generated method stub
-		
+		recuperaAtividade(idTarefa).adicionarHorasTarefa(idTarefa, horas);
 	}
 
 	public void removerHorasTarefa(String idTarefa, int horas) {
-		// TODO Auto-generated method stub
-		
+		recuperaAtividade(idTarefa).removerHorasTarefa(idTarefa, horas);
+
 	}
 
 	public void concluirTarefa(String idTarefa) {
-		// TODO Auto-generated method stub
-		
+		recuperaAtividade(idTarefa).concluirTarefa(idTarefa);
+
 	}
 
 	public void removerTarefa(String idTarefa) {
-		// TODO Auto-generated method stub
-		
+		recuperaAtividade(idTarefa).removerTarefa(idTarefa);
+
 	}
 
 	public String exibirTarefa(String idTarefa) {
-		// TODO Auto-generated method stub
-		return null;
+		return recuperaAtividade(idTarefa).exibirTarefa(idTarefa);
 	}
 
 	public void associarPessoaTarefa(String cpf, String idTarefa) {
-		// TODO Auto-generated method stub
-		
+		recuperaAtividade(idTarefa).associarPessoaTarefa(idTarefa, ps.getPessoa(cpf));
+
 	}
 
 	public void removerPessoaTarefa(String cpf, String idTarefa) {
-		// TODO Auto-generated method stub
-		
+		recuperaAtividade(idTarefa).removerPessoaTarefa(cpf, idTarefa);
+
 	}
-	
-	public void recuperaIdAtividade(String idTarefa) {
-		// TODO
+
+	private String recuperaIdAtividade(String idTarefa) {
+		int count = 0;
+		String idAtividade = "";
+		String[] caracteres = idTarefa.split("");
+		for (int i = 0; i < caracteres.length; i++) {
+			if (caracteres[i].equals("-")) {
+				count++;
+			}
+			if (count < 2) {
+				idAtividade += caracteres[i];
+			}
+		}
+
+		return idAtividade;
 	}
-	
-	
+
+	private Atividade recuperaAtividade(String idTarefa) {
+		return atividades.get(recuperaIdAtividade(idTarefa));
+	}
 
 }
