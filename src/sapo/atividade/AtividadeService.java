42b1e984beab85b0dc09de26d9f535d02a36c5ed
Sabrinabs <sabrina.silva@ccc.ufcg.edu.br>
2022-08-12 11:02:00 -0300

    assinando os metodos de Tarefa

--- src/sapo/atividade/AtividadeService.java
+++ src/sapo/atividade/AtividadeService.java
@@ -9,4 +9,74 @@ public class AtividadeService {
         this.ps = ps;
     }
 
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+   
+    // tarefa
+	public String cadastraTarefa(String atividadeId, String nome, String[] habilidades) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public void alterarNomeTarefa(String tarefaId, String novoNome) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void alterarHabilidadesTarefa(String idTarefa, String[] habilidades) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void adicionarHorasTarefa(String idTarefa, int horas) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void removerHorasTarefa(String idTarefa, int horas) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void concluirTarefa(String idTarefa) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void removerTarefa(String idTarefa) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public String exibirTarefa(String idTarefa) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public void associarPessoaTarefa(String cpf, String idTarefa) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void removerPessoaTarefa(String cpf, String idTarefa) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	public void recuperaIdAtividade(String idTarefa) {
+		// TODO
+	}
+	
+	
+
 }
