c2e1576c29d1428de2e0437cdce78cae428ddccf
Sabrinabs <sabrina.silva@ccc.ufcg.edu.br>
2022-08-13 00:12:33 -0300

    refatorando

--- src/sapo/atividade/Atividade.java
+++ src/sapo/atividade/Atividade.java
@@ -6,71 +6,10 @@ import sapo.pessoa.Pessoa;
 import sapo.tarefa.Tarefa;
 
 public class Atividade {
-	private Map<String, Tarefa> tarefas;
-	private int qntTarefas;
 	
 
 	
 	
 	
-	
-	
-	
-	
-	
-	
-	// tarefa
-	
-	private String geraIdTarefa(){
-		return this.id += "-" + qntTarefas;
-	}
-	
-	public String cadastraTarefa(String nome, String[] habilidades) {
-		String idTarefa = geraIdTarefa();
-		tarefas.put(idTarefa, new Tarefa(idTarefa, nome, habilidades));
-		qntTarefas ++;
-		return  idTarefa;
-	}
 
-	public void alteraNomeTarefa(String idTarefa, String novoNome) {
-		tarefas.get(idTarefa).setNome(novoNome);
-	}
-
-	public void alterarHabilidadesTarefa(String idTarefa, String[] habilidades) {
-		tarefas.get(idTarefa).setHabilidades(habilidades);
-		
-	}
-
-	public void adicionarHorasTarefa(String idTarefa, int horas) {
-		tarefas.get(idTarefa).adicionaHoras(horas);		
-	}
-
-	public void removerHorasTarefa(String idTarefa, int horas) {
-		tarefas.get(idTarefa).removeHoras(horas);
-		
-	}
-
-	public void concluirTarefa(String idTarefa) {
-		tarefas.get(idTarefa).concluirTarefa();
-		
-	}
-
-	public void removerTarefa(String idTarefa) {
-		tarefas.remove(idTarefa);
-		
-	}
-
-	public String exibirTarefa(String idTarefa) {
-		return tarefas.get(idTarefa).toString();
-		
-	}
-
-	public void associarPessoaTarefa(String idTarefa, Pessoa pessoa) {
-		tarefas.get(idTarefa).adicionaResponsavel(pessoa);
-		
-	}
-	
-	public void removerPessoaTarefa(String cpf, String idTarefa) {
-		tarefas.get(idTarefa).removerResponsavel(cpf);		
-	}
 }
--- src/sapo/atividade/AtividadeService.java
+++ src/sapo/atividade/AtividadeService.java
@@ -1,6 +1,5 @@
 package sapo.atividade;
 
-import java.util.Map;
 
 import sapo.pessoa.PessoaService;
 
@@ -12,71 +11,5 @@ public class AtividadeService {
 	}
 
 	
-	// tarefa
-	public String cadastraTarefa(String atividadeId, String nome, String[] habilidades) {
-		return atividades.get(atividadeId).cadastraTarefa(nome, habilidades);
-	}
-
-	public void alterarNomeTarefa(String tarefaId, String novoNome) {
-		recuperaAtividade(tarefaId).alteraNomeTarefa(tarefaId, novoNome);
-
-	}
-
-	public void alterarHabilidadesTarefa(String idTarefa, String[] habilidades) {
-		recuperaAtividade(idTarefa).alterarHabilidadesTarefa(idTarefa, habilidades);
-	}
-
-	public void adicionarHorasTarefa(String idTarefa, int horas) {
-		recuperaAtividade(idTarefa).adicionarHorasTarefa(idTarefa, horas);
-	}
-
-	public void removerHorasTarefa(String idTarefa, int horas) {
-		recuperaAtividade(idTarefa).removerHorasTarefa(idTarefa, horas);
-
-	}
-
-	public void concluirTarefa(String idTarefa) {
-		recuperaAtividade(idTarefa).concluirTarefa(idTarefa);
-
-	}
-
-	public void removerTarefa(String idTarefa) {
-		recuperaAtividade(idTarefa).removerTarefa(idTarefa);
-
-	}
-
-	public String exibirTarefa(String idTarefa) {
-		return recuperaAtividade(idTarefa).exibirTarefa(idTarefa);
-	}
-
-	public void associarPessoaTarefa(String cpf, String idTarefa) {
-		recuperaAtividade(idTarefa).associarPessoaTarefa(idTarefa, ps.getPessoa(cpf));
-
-	}
-
-	public void removerPessoaTarefa(String cpf, String idTarefa) {
-		recuperaAtividade(idTarefa).removerPessoaTarefa(cpf, idTarefa);
-
-	}
-
-	private String recuperaIdAtividade(String idTarefa) {
-		int count = 0;
-		String idAtividade = "";
-		String[] caracteres = idTarefa.split("");
-		for (int i = 0; i < caracteres.length; i++) {
-			if (caracteres[i].equals("-")) {
-				count++;
-			}
-			if (count < 2) {
-				idAtividade += caracteres[i];
-			}
-		}
-
-		return idAtividade;
-	}
-
-	private Atividade recuperaAtividade(String idTarefa) {
-		return atividades.get(recuperaIdAtividade(idTarefa));
-	}
 
 }
