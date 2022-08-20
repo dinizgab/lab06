b886906762719935f6a298f7f771754db7c82861
João Victor de Souza Lucena <joaovictordev2003@gmail.com>
2022-08-20 00:48:42 -0300

    Alterando Pessoa pós implementação de Função

--- src/sapo/pessoa/Pessoa.java
+++ src/sapo/pessoa/Pessoa.java
@@ -3,65 +3,122 @@ package sapo.pessoa;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+
+import sapo.funcao.Funcao;
+import sapo.tarefa.heranca.TarefaAbstract;
 
 public class Pessoa {
-    private final String cpf;
-    private String nome;
-    private String[] habilidades;
-    private final List<Comentario> comentarios;
-    public Pessoa(String cpf, String nome, String[] habilidades) {
-        this.cpf = cpf;
-        this.nome = nome;
-        this.habilidades = habilidades;
-        this.comentarios = new ArrayList<>();
+  private final String cpf;
+  private String nome;
+  private String[] habilidades;
+  private final List<Comentario> comentarios;
+  private Map<String, TarefaAbstract> trfsQueAfetamNivel;
+  private Funcao func;
+  private int nivel;
+
+  public Pessoa(String cpf, String nome, String[] habilidades, Funcao func) {
+    this.cpf = cpf;
+    this.nome = nome;
+    this.habilidades = habilidades;
+    this.comentarios = new ArrayList<>();
+    this.trfsQueAfetamNivel = new HashMap<>();
+    this.func = func;
+    this.nivel = 0;
+  }
+
+  public int getNivel() {
+    if (func == null) {
+      int concluidas = 0;
+      for (TarefaAbstract tarefa : trfsQueAfetamNivel.values()) {
+        if (tarefa.getStatus())
+          concluidas++;
+      }
+      return trfsQueAfetamNivel.size() / 2 + concluidas + this.nivel;
     }
 
-    public String getNome() {
-        return this.nome;
-    }
-    public void alteraNome(String nome) {
-        this.nome = nome;
-    }
-    public String getCpf() {
-        return this.cpf;
+    return func.calcNivel(trfsQueAfetamNivel, habilidades) + this.nivel;
+  }
+
+  public void setFuncao(Funcao func) throws IllegalStateException {
+    if (func == null && this.func == null) {
+      throw new IllegalStateException("Não é possível remover a função da pessoa de cpf "
+          + this.cpf + ", pois ela não possui função.");
     }
 
-    public void alteraHabilidades(String[] habilidades) {
-        this.habilidades = habilidades;
+    if (this.func.getClass() == func.getClass()) {
+      throw new IllegalStateException("Não é possível definir a função de " + func.getClass() + " para a pessoa de cpf "
+          + this.cpf + ", pois ela já assume essa função.");
     }
 
-    public void adicionaComentario(Comentario comentario) {
-        this.comentarios.add(comentario);
+    this.nivel += func.calcNivel(trfsQueAfetamNivel, habilidades);
+    this.trfsQueAfetamNivel = new HashMap<>();
+    this.func = func;
+  }
+
+  public void addTarefa(TarefaAbstract novaTarefa) {
+    trfsQueAfetamNivel.put(novaTarefa.getCodigo(), novaTarefa);
+  }
+
+  public void removeTarefa(String codigo) {
+    trfsQueAfetamNivel.remove(codigo);
+  }
+
+  public Map<String, TarefaAbstract> getTarefas() {
+    return trfsQueAfetamNivel;
+  }
+
+  public String getNome() {
+    return this.nome;
+  }
+
+  public void alteraNome(String nome) {
+    this.nome = nome;
+  }
+
+  public String getCpf() {
+    return this.cpf;
+  }
+
+  public void alteraHabilidades(String[] habilidades) {
+    this.habilidades = habilidades;
+  }
+
+  public void adicionaComentario(Comentario comentario) {
+    this.comentarios.add(comentario);
+  }
+
+  public String getComentarios() {
+    String listaComentarios = formataComentarios();
+    return this.nome + " - " + this.cpf + "Comentários:\n" + listaComentarios;
+  }
+
+  private String formataComentarios() {
+    String listaDeComentarios = "";
+    Collections.sort(this.comentarios);
+    for (Comentario comentario : this.comentarios) {
+      listaDeComentarios += comentario.toString() + "\n";
     }
 
-    public String getComentarios() {
-        String listaComentarios = formataComentarios();
-        return this.nome + " - " + this.cpf + "Comentários:\n" + listaComentarios;
-    }
+    return listaDeComentarios;
+  }
 
-    private String formataComentarios() {
-        String listaDeComentarios = "";
-        Collections.sort(this.comentarios);
-        for(Comentario comentario : this.comentarios) {
-            listaDeComentarios += comentario.toString() + "\n";
-        }
+  private String formataHabilidades() {
+    Arrays.sort(this.habilidades);
+    String habilidadesFormatadas = "";
 
-        return listaDeComentarios;
+    for (String habilidade : this.habilidades) {
+      habilidadesFormatadas += habilidade;
     }
+    return habilidadesFormatadas;
+  }
 
-    private String formataHabilidades() {
-        Arrays.sort(this.habilidades);
-        String habilidadesFormatadas = "";
-
-        for(String habilidade : this.habilidades) {
-            habilidadesFormatadas += habilidade;
-        }
-        return habilidadesFormatadas;
-    }
-    @Override
-    public String toString() {
-        String habilidades = formataHabilidades();
-        return this.nome + " - " + this.cpf + "\n" + habilidades;
-    }
+  @Override
+  public String toString() {
+    String habilidades = formataHabilidades();
+    String funcDescription = this.func != null ? this.func.toString() + "\n" : "";
+    return this.nome + " - " + this.cpf + "\n" + funcDescription + habilidades;
+  }
 }
