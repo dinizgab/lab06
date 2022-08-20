cfb6dcf36472e7b69033b1bec537cdb066d4200f
Sabrinabs <sabrina.silva@ccc.ufcg.edu.br>
2022-08-20 11:40:18 -0300

    implementando busca

--- src/sapo/busca/BuscaAbstract.java
+++ src/sapo/busca/BuscaAbstract.java
@@ -3,11 +3,15 @@ package sapo.busca;
 import java.util.List;
 
 public abstract class BuscaAbstract implements BuscaInterface{
+	/**
+	 * tipo de busca realizada
+	 */
 	private String tipo;
 	
 	public BuscaAbstract(String tipo){
 		this.tipo = tipo;
 	}
+	
 	@Override
 	public abstract List<String> busca();
 
