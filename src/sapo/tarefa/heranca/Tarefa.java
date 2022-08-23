8e194e1ee8ba83ffbe295cb8aef35da88eb2de15
João Victor de Souza Lucena <joaovictordev2003@gmail.com>
2022-08-23 18:57:28 -0300

    Ajeitando parâmetros de inicialização

--- src/sapo/tarefa/heranca/Tarefa.java
+++ src/sapo/tarefa/heranca/Tarefa.java
@@ -2,38 +2,37 @@ package sapo.tarefa.heranca;
 
 import java.util.Set;
 
-import sapo.atividade.Atividade;
-
 public class Tarefa extends TarefaAbstract {
-    private Set<String> habilidades;
 
-    /**
-     * cria uma tarefa definindo seu codigo, nome, habilidade e a atividade a qual
-     * esta relacionada.
-     *
-     * @param codigo      codigo da tarefa
-     * @param nome        nome da tarefa
-     * @param habilidades habilidades recomendadas da tarefa
-     * @param atividade   atividade relacionada
-     */
-    public Tarefa(String nome, String codigo, Atividade atividade, Set<String> habilidades) {
-        super(nome, codigo, atividade);
-        this.habilidades = habilidades;
-    }
+  /**
+   * cria uma tarefa definindo seu codigo, nome, habilidade e a atividade a qual
+   * esta relacionada.
+   *
+   * @param codigo      codigo da tarefa
+   * @param nome        nome da tarefa
+   * @param habilidades habilidades recomendadas da tarefa
+   * @param atividade   atividade relacionada
+   */
+  public Tarefa(String nome, String codigo, String atividadeNome, Set<String> habilidades) {
+    super(nome, codigo, atividadeNome);
+    this.habilidades = habilidades;
+  }
 
-    /**
-     * define as habilidades recomendadas da tarefa.
-     *
-     * @param habilidades habilidades da tarefa.
-     */
-    public void setHabilidades(Set<String> habilidades) {
-        if (!this.concluida) return;
-        this.habilidades = habilidades;
-    }
+  /**
+   * define as habilidades recomendadas da tarefa.
+   *
+   * @param habilidades habilidades da tarefa.
+   */
+  public void setHabilidades(Set<String> habilidades) {
+    if (!this.concluida)
+      return;
+    this.habilidades = habilidades;
+  }
 
-    @Override
-    public String toString() {
-        return this.nome + " - " + codigo + "\n- " + atividade.getNome() + "\n" + this.exibeHabilidades() + "\n(" + this.horas
-                + " hora(s) executada(s))" + "\n===\n" + "Equipe:\n" + this.exibeEquipe();
-    }
+  @Override
+  public String toString() {
+    return this.nome + " - " + codigo + "\n- " + atividadeNome + "\n" + this.exibeHabilidades() + "\n("
+        + this.horas
+        + " hora(s) executada(s))" + "\n===\n" + "Equipe:\n" + this.exibeEquipe();
+  }
 }
--- src/sapo/tarefa/heranca/TarefaGerencial.java
+++ src/sapo/tarefa/heranca/TarefaGerencial.java
@@ -1,80 +1,82 @@
 package sapo.tarefa.heranca;
 
-import sapo.atividade.Atividade;
 import sapo.tarefa.ValidadorTarefa;
 
 import java.util.*;
 
 public class TarefaGerencial extends TarefaAbstract {
-    private Set<String> habilidades;
-    private Map<String, TarefaAbstract> tarefasGerenciadas;
-    private int horas;
-    private ValidadorTarefa validador;
+  private Set<String> habilidades;
+  private Map<String, TarefaAbstract> tarefasGerenciadas;
+  private int horas;
+  private ValidadorTarefa validador;
 
-    public TarefaGerencial(String nome, String codigo, Atividade atividade, Set<String> habilidades, Map<String, TarefaAbstract> tarefas) {
-        super(nome, codigo, atividade);
+  public TarefaGerencial(String nome, String codigo, String atividadeNome, Set<String> habilidades,
+      Map<String, TarefaAbstract> tarefas) {
+    super(nome, codigo, atividadeNome);
 
-        this.validador = new ValidadorTarefa();
-        this.tarefasGerenciadas = tarefas;
-        this.habilidades = this.criaHabilidadesTotais(habilidades);
-        this.horas = this.calculaHorasTotais();
+    this.validador = new ValidadorTarefa();
+    this.tarefasGerenciadas = tarefas;
+    this.habilidades = this.criaHabilidadesTotais(habilidades);
+    this.horas = this.calculaHorasTotais();
+  }
+
+  @Override
+  public void concluiTarefa() {
+    for (TarefaAbstract t : this.tarefasGerenciadas.values()) {
+      t.concluiTarefa();
+    }
+    this.concluida = true;
+  }
+
+  @Override
+  public String toString() {
+    return this.nome + " - " + codigo + "\n- " + atividadeNome + "\n" + this.exibeHabilidades() + "\n(" + this.horas
+        + " hora(s) executada(s))" + "\n===\n" + "Equipe:\n" + this.exibeEquipe() + "\n===\nTarefas:\n"
+        + this.exibeTarefas();
+  }
+
+  private Set<String> criaHabilidadesTotais(Set<String> habilidades) {
+    for (TarefaAbstract t : this.tarefasGerenciadas.values()) {
+      habilidades.addAll(t.getHabilidades());
+    }
+    return habilidades;
+  }
+
+  private int calculaHorasTotais() {
+    int horas = 0;
+    for (TarefaAbstract t : this.tarefasGerenciadas.values()) {
+      horas += t.getHoras();
     }
 
-    @Override
-    public void concluiTarefa() {
-        for (TarefaAbstract t : this.tarefasGerenciadas.values()) {
-            t.concluiTarefa();
-        }
-        this.concluida = true;
+    return horas;
+  }
+
+  private String exibeTarefas() {
+    List<String> listaCodigos = new ArrayList<>(this.tarefasGerenciadas.keySet());
+    listaCodigos.sort(Collections.reverseOrder());
+
+    String saida = "";
+    for (String cods : listaCodigos) {
+      saida += "- " + this.tarefasGerenciadas.get(cods).getNome() + this.tarefasGerenciadas.get(cods).getCodigo()
+          + "\n";
     }
 
-    @Override
-    public String toString() {
-        return this.nome + " - " + codigo + "\n- " + atividade.getNome() + "\n" + this.exibeHabilidades() + "\n(" + this.horas
-                + " hora(s) executada(s))" + "\n===\n" + "Equipe:\n" + this.exibeEquipe() + "\n===\nTarefas:\n" + this.exibeTarefas();
-    }
+    return saida;
+  }
 
-    private Set<String> criaHabilidadesTotais(Set<String> habilidades) {
-        for (TarefaAbstract t : this.tarefasGerenciadas.values()) {
-            habilidades.addAll(t.getHabilidades());
-        }
-        return habilidades;
-    }
+  public void adicionaTarefaGerenciada(TarefaAbstract tarefa) {
+    validador.validaCiclo(this.tarefasGerenciadas, tarefa);
 
-    private int calculaHorasTotais() {
-        int horas = 0;
-        for (TarefaAbstract t : this.tarefasGerenciadas.values()) {
-            horas += t.getHoras();
-        }
+    this.tarefasGerenciadas.put(tarefa.getCodigo(), tarefa);
+    this.criaHabilidadesTotais(this.habilidades);
+  }
 
-        return horas;
-    }
+  public void removeTarefaGerenciada(String codigo) {
+    this.tarefasGerenciadas.remove(codigo);
+    this.criaHabilidadesTotais(this.habilidades);
+  }
 
-    private String exibeTarefas() {
-        List<String> listaCodigos = new ArrayList<>(this.tarefasGerenciadas.keySet());
-        listaCodigos.sort(Collections.reverseOrder());
-
-        String saida = "";
-        for (String cods : listaCodigos) {
-            saida += "- " + this.tarefasGerenciadas.get(cods).getNome() + this.tarefasGerenciadas.get(cods).getCodigo() + "\n";
-        }
-
-        return saida;
-    }
-
-    public void adicionaTarefaGerenciada(TarefaAbstract tarefa) {
-        validador.validaCiclo(this.tarefasGerenciadas, tarefa);
-
-        this.tarefasGerenciadas.put(tarefa.getCodigo(), tarefa);
-        this.criaHabilidadesTotais(this.habilidades);
-    }
-
-    public void removeTarefaGerenciada(String codigo) {
-        this.tarefasGerenciadas.remove(codigo);
-        this.criaHabilidadesTotais(this.habilidades);
-    }
-
-    public int totalDeTarefas() {
-        return this.tarefasGerenciadas.size();
-    }
+  public int totalDeTarefas() {
+    return this.tarefasGerenciadas.size();
+  }
 }
