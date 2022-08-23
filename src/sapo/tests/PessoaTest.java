c7c340d93f47d65e574e979514f588c0de47905b
Sabrinabs <sabrina.silva@ccc.ufcg.edu.br>
2022-08-22 22:50:38 -0300

    testes da funcionalidade Pessoa

--- src/sapo/tests/PessoaTest.java
+++ src/sapo/tests/PessoaTest.java
@@ -72,9 +72,9 @@ class PessoaTest extends BaseTest{
 	void testAdicionarEListarComentarios() {
 		pc.cadastraPessoa("111.111.111-11", "Ada Lovelace", this.preparaHabilidades());
 		pc.cadastraPessoa("222.222.222-22", "Alan Turing", this.preparaHabilidades());
-		pc.adicionarComentario("111.111.111-11", "uma grande referencia no campo de computação", "222.222.222-22");
-		String listaDeComentarios = pc.listarComentarios("222.222.222-22");
-		assertEquals("Alan Turing - 222.222.222-22Comentários:\n", listaDeComentarios);
+		pc.adicionarComentario("111.111.111-11", "uma grande referencia no campo da computação", "222.222.222-22");
+		String listaDeComentarios = pc.listarComentarios("111.111.111-11");
+		assertEquals("Ada Lovelace - 111.111.111-11Comentários:\numa grande referencia no campo da computação (Alan Turing)\n", listaDeComentarios);
 	}
 	
 	@Test
