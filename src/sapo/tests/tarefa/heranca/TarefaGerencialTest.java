92201591b9c93be114dd09d01740b1dc10b930b2
Gabriel Diniz <gabrielpombodiniz@gmail.com>
2022-08-24 00:09:40 -0300

    final

--- src/sapo/tests/tarefa/heranca/TarefaGerencialTest.java
+++ src/sapo/tests/tarefa/heranca/TarefaGerencialTest.java
@@ -1,8 +1,6 @@
 package sapo.tests.tarefa.heranca;
 
-import static org.junit.Assert.assertThrows;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 
 import java.util.Arrays;
 import java.util.HashMap;
