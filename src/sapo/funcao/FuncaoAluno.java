66e0666284bb79b8b0f8b0297887f1240b15d2ea
João Victor de Souza Lucena <joaovictordev2003@gmail.com>
2022-08-23 21:46:03 -0300

    Corrige cálculo do nível ao envolver tarefas gerenciais

--- src/sapo/funcao/FuncaoAluno.java
+++ src/sapo/funcao/FuncaoAluno.java
@@ -6,39 +6,48 @@ import java.util.Map;
 import java.util.Set;
 
 import sapo.tarefa.heranca.TarefaAbstract;
+import sapo.tarefa.heranca.TarefaGerencial;
 
 public class FuncaoAluno implements Funcao {
 
-    private String matricula;
-    private int periodo;
+  private String matricula;
+  private int periodo;
 
-    public FuncaoAluno(String matricula, int periodo) {
-        this.matricula = matricula;
-        this.periodo = periodo;
+  public FuncaoAluno(String matricula, int periodo) {
+    this.matricula = matricula;
+    this.periodo = periodo;
+  }
+
+  @Override
+  public int calcNivel(Map<String, TarefaAbstract> tarefas, String[] habilidades) {
+    int concluidas = 0;
+    int concluidasComHabilidade = 0;
+    int pendente = 0;
+    for (TarefaAbstract tarefa : tarefas.values()) {
+      boolean ehGerencial = tarefa instanceof TarefaGerencial;
+      int valorEmQuantTrf = 1;
+      if (ehGerencial)
+        valorEmQuantTrf = 2;
+
+      if (tarefa.getStatus()) {
+        Set<String> clone = new HashSet<>(tarefa.getHabilidades());
+        clone.retainAll(Arrays.asList(habilidades));
+
+        if (clone.size() != 0)
+          concluidasComHabilidade += valorEmQuantTrf;
+        else
+          concluidas += valorEmQuantTrf;
+      }
+
+      pendente += valorEmQuantTrf;
+
     }
+    return pendente / 2 + (int) Math.ceil(1.5 * concluidasComHabilidade) + concluidas;
+  }
 
-    @Override
-    public int calcNivel(Map<String, TarefaAbstract> tarefas, String[] habilidades) {
-        int concluidas = 0;
-        int concluidasComHabilidade = 0;
-        for (TarefaAbstract tarefa : tarefas.values()) {
-            if (tarefa.getStatus()) {
-                Set<String> clone = new HashSet<>(tarefa.getHabilidades());
-                clone.retainAll(Arrays.asList(habilidades));
-
-                if (clone.size() != 0)
-                    concluidasComHabilidade++;
-                else
-                    concluidas++;
-            }
-        }
-        int pendente = tarefas.size() - concluidas - concluidasComHabilidade;
-        return pendente / 2 + (int) Math.ceil(1.5 * concluidasComHabilidade) + concluidas;
-    }
-
-    @Override
-    public String toString() {
-        return "Aluno - " + matricula + " - " + periodo;
-    }
+  @Override
+  public String toString() {
+    return "Aluno - " + matricula + " - " + periodo;
+  }
 
 }
